5d2ea00f67a97e60b497ca00bd1652f79d1899b4
Jlferrete <joseluisferrete@gmail.com>
2020-09-10 19:08:49 +0200

    Crumb fixed

--- crumb.sh
+++ crumb.sh
@@ -6,4 +6,4 @@ CRUMB=$(curl -s 'jenkins.local:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequ
 
 #curl -s -XPOST 'http://jenkins.local:8080/job/ENV/build?delay=0sec' -u jenkins:1ffb8250a970e2c6b4feaaef1089b73c -H "$CRUMB" -H "Content-Type:text/xml"
 
-curl -s -XPOST 'http://jenkins.local:8080/job/backup-db-to-aws/buildWithParameters?DB_HOST=db_host&DB_NAME=testdb&BUCKET_NAME=jlferrete-jenkins-udemy' -u jenkins:1ffb8250a970e2c6b4feaaef1089b73c -H "$CRUMB" -H "Content-Type:text/xml"
+curl -s -XPOST 'http://jenkins.local:8080/job/backup-db-to-aws/buildWithParameters?DB_HOST=db_host&DB_NAME=testdb&BUCKET_NAME=jlferrete-jenkins-udemy2' -u jenkins:1ffb8250a970e2c6b4feaaef1089b73c -H "$CRUMB" -H "Content-Type:text/xml"
